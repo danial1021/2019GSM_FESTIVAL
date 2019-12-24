a11e75200e8fa5403b75bff09e1779a4f8f11e4f
EUBIN <41097199+danial1021@users.noreply.github.com>
2019-12-24 16:10:35 +0900

    Adding User Profile Modify feature
    Preparing Profile Preview

--- be/routes/api/user/index.js
+++ be/routes/api/user/index.js
@@ -1,11 +1,21 @@
+  
 const router = require('express').Router()
 const createError = require('http-errors')
 const multer = require('multer')
+const sharp = require('sharp')
+const imageDataURI = require('image-data-uri')
+const fs = require('fs')
 
-router.post('/', multer({ dest: 'public'}).single('bin'), (req, res, next) => {
-  console.log(req.body);
-  console.log(req.file);
-  res.status(204).send()
+router.post('/', multer({ dest: 'public/' }).single('bin') ,(req, res, next) => {
+  console.log(req.body)
+  console.log(req.file)
+
+  sharp(req.file.path).resize(200,200).crop(sharp.strategy.entropy).toBuffer()
+    .then(bf => {
+      fs.unlinkSync(req.file.path)
+      res.send(imageDataURI.encode(bf, 'png'))
+    })
+    .catch(e => next(e))
 })
 
-module.exports = router;
+module.exports = router
--- fe/src/views/user.vue
+++ fe/src/views/user.vue
@@ -1,38 +1,45 @@
 <template>
-  <v-container>
-    <div>
-      <v-card>
-          <li>
-            <td>ddd</td>
-            <td>ddd</td>
-          </li>
-          <h1>dfddf</h1>
-          <strike>dfdfdfd</strike>
-        <v-card-title>회원 정보 수정</v-card-title>
-        <v-card-text>
-          <input id="bin" type="file" >
-        </v-card-text>
-        <v-card-actions>
-          <v-btn @click="upload">올림</v-btn>
-        </v-card-actions>
-      </v-card>
-    </div>
+  <v-container fluid fill-height>
+    <v-layout align-center justify-center>
+      <v-flex xs12 sm8 md4>
+        <v-card class="elevation-12">
+          <v-card-title>사용 프로필 수정</v-card-title>
+          <v-card-text>
+            <input id="bin" type="file">
+            <img :src="img"/>
+          </v-card-text>
+          <v-card-actions>
+            <v-btn @click="upload">전송</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-flex>
+    </v-layout>
   </v-container>
 </template>
+
 <script>
 export default {
+  data: () => ({
+    form: {
+      name: 'xx'
+    },
+    img: 'http://medfordgospelmission.org/wp-content/uploads/2018/06/default-user-image-female.png'
+  }),
   mounted () {
   },
   methods: {
     upload () {
       const fd = new FormData()
-      fd.append('name', 'xxxx')
+      fd.append('name', this.form.name)
       fd.append('bin', document.getElementById('bin').files[0])
       this.$axios.post('/user', fd)
-        .then(r => {
-          console.log('success')
+        .then(({ data }) => {
+          this.img = data
+          this.$store.commit('pop', { msg: '파일 업로드 완료', color: 'success' })
         })
-        .catch(e => console.log(e.message))
+        .catch(e => {
+          if (!e.response) this.$store.commit('pop', { msg: e.message, color: 'warning' })
+        })
     }
   }
 }
