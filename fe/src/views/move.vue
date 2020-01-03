3d409cddac3fb8cb7518c2ed6ca447fccdfe06c2
EUBIN <41097199+danial1021@users.noreply.github.com>
2020-01-03 14:50:28 +0900

    Complete the Move Page
    Complete the Move Page
    on the FrontEnd

--- fe/src/views/move.vue
+++ fe/src/views/move.vue
@@ -35,7 +35,7 @@
     </v-stepper-content>
     <v-stepper-step :complete="e6 > 3" step="3">머무를 시간 정하기</v-stepper-step>
     <v-stepper-content step="3">
-      <v-card color="grey lighten-5" class="mb-5" height="100px" ref="form">
+      <v-card color="grey lighten-5" class="mb-5" height="150px" ref="form">
         <v-card-text>
           <v-layout row wrap>
             <v-flex xs11 sm5>
@@ -99,7 +99,7 @@
         <v-card-text>
           <v-flex xs12>
             <v-text-field
-              label="이동 사유"
+              label="이동 사유를 작성해주세요"
               textarea
               :counter="1000"
               :rules="[(v) => v.length <= 1000 || 'Max 1000 characters']"
@@ -112,7 +112,7 @@
       </v-card>
       <v-btn color="primary" @click.native="e6 = 1">Return to the beginning</v-btn>
       <v-btn flat @click.native="e6 = 3">Backward</v-btn>
-      <v-btn color="primary" flat @click="check">Submit</v-btn>
+      <v-btn color="primary" flat @click="submit">Submit</v-btn>
     </v-stepper-content>
   </v-stepper>
 </template>
@@ -128,14 +128,36 @@ export default {
     },
     menu2: false,
     modal2: false,
-    locations: ['Afghanistan', 'Albania', 'Algeria', 'Andorra', 'Angola', 'Anguilla', 'Antigua &amp; Barbuda', 'Argentina', 'Armenia', 'Aruba', 'Australia', 'Austria', 'Azerbaijan', 'Bahamas', 'Bahrain', 'Bangladesh', 'Barbados', 'Belarus', 'Belgium', 'Belize', 'Benin', 'Bermuda', 'Bhutan', 'Bolivia', 'Bosnia &amp; Herzegovina', 'Botswana', 'Brazil', 'British Virgin Islands', 'Brunei', 'Bulgaria', 'Burkina Faso', 'Burundi', 'Cambodia', 'Cameroon', 'Cape Verde', 'Cayman Islands', 'Chad', 'Chile', 'China', 'Colombia', 'Congo', 'Cook Islands', 'Costa Rica', 'Cote D Ivoire', 'Croatia', 'Cruise Ship', 'Cuba', 'Cyprus', 'Czech Republic', 'Denmark', 'Djibouti', 'Dominica', 'Dominican Republic', 'Ecuador', 'Egypt', 'El Salvador', 'Equatorial Guinea', 'Estonia', 'Ethiopia', 'Falkland Islands', 'Faroe Islands', 'Fiji', 'Finland', 'France', 'French Polynesia', 'French West Indies', 'Gabon', 'Gambia', 'Georgia', 'Germany', 'Ghana', 'Gibraltar', 'Greece', 'Greenland', 'Grenada', 'Guam', 'Guatemala', 'Guernsey', 'Guinea', 'Guinea Bissau', 'Guyana', 'Haiti', 'Honduras', 'Hong Kong', 'Hungary', 'Iceland', 'India', 'Indonesia', 'Iran', 'Iraq', 'Ireland', 'Isle of Man', 'Israel', 'Italy', 'Jamaica', 'Japan', 'Jersey', 'Jordan', 'Kazakhstan', 'Kenya', 'Kuwait', 'Kyrgyz Republic', 'Laos', 'Latvia', 'Lebanon', 'Lesotho', 'Liberia', 'Libya', 'Liechtenstein', 'Lithuania', 'Luxembourg', 'Macau', 'Macedonia', 'Madagascar', 'Malawi', 'Malaysia', 'Maldives', 'Mali', 'Malta', 'Mauritania', 'Mauritius', 'Mexico', 'Moldova', 'Monaco', 'Mongolia', 'Montenegro', 'Montserrat', 'Morocco', 'Mozambique', 'Namibia', 'Nepal', 'Netherlands', 'Netherlands Antilles', 'New Caledonia', 'New Zealand', 'Nicaragua', 'Niger', 'Nigeria', 'Norway', 'Oman', 'Pakistan', 'Palestine', 'Panama', 'Papua New Guinea', 'Paraguay', 'Peru', 'Philippines', 'Poland', 'Portugal', 'Puerto Rico', 'Qatar', 'Reunion', 'Romania', 'Russia', 'Rwanda', 'Saint Pierre &amp; Miquelon', 'Samoa', 'San Marino', 'Satellite', 'Saudi Arabia', 'Senegal', 'Serbia', 'Seychelles', 'Sierra Leone', 'Singapore', 'Slovakia', 'Slovenia', 'South Africa', 'South Korea', 'Spain', 'Sri Lanka', 'St Kitts &amp; Nevis', 'St Lucia', 'St Vincent', 'St. Lucia', 'Sudan', 'Suriname', 'Swaziland', 'Sweden', 'Switzerland', 'Syria', 'Taiwan', 'Tajikistan', 'Tanzania', 'Thailand', "Timor L'Este", 'Togo', 'Tonga', 'Trinidad &amp; Tobago', 'Tunisia', 'Turkey', 'Turkmenistan', 'Turks &amp; Caicos', 'Uganda', 'Ukraine', 'United Arab Emirates', 'United Kingdom', 'United States', 'Uruguay', 'Uzbekistan', 'Venezuela', 'Vietnam', 'Virgin Islands (US)', 'Yemen', 'Zambia', 'Zimbabwe'],
+    locations: [
+      'Afghanistan', 'Albania', 'Algeria', 'Andorra', 'Angola', 'Anguilla', 'Antigua &amp; Barbuda',
+      'Argentina', 'Armenia', 'Aruba', 'Australia', 'Austria', 'Azerbaijan', 'Bahamas',
+      'Bahrain', 'Bangladesh', 'Barbados', 'Belarus', 'Belgium', 'Belize', 'Benin',
+      'Bermuda', 'Bhutan', 'Bolivia', 'Bosnia &amp; Herzegovina', 'Botswana', 'Brazil', 'British Virgin Islands'
+    ],
     e6: 1
   }),
 
   methods: {
     submit () {
+      this.$validator.validateAll()
+        .then(r => {
+          if (!r) throw new Error('모두 기입해주세요')
+          else this.$axios.post('move', this.form)
+        })
+        .then(r => {
+          if (!r.data.success) throw new Error(r.data.msg)
+          this.$store.commit('pop', { msg: '신청이 완료 되었습니다', color: 'success' })
+          this.$router.push('/')
+        })
+        .catch(e => {
+          if (!e.response) this.$store.commit('pop', { msg: e.message, color: 'warning' })
+        })
     },
-
+    pop (m, cl) {
+      this.sb.act = true
+      this.sb.msg = m
+      this.sb.color = cl
+    },
     check () {
       console.log(this.form)
     }
