53e72cacbc3d7f9e0d153386ef89bf39131e28f2
alphacaQ <hess007@naver.com>
2020-01-06 21:15:05 +0900

    preparing 1floor
    preparing 1floor

--- fe/src/views/floor/1floor.vue
+++ fe/src/views/floor/1floor.vue
@@ -1,7 +1,209 @@
 <template>
-  <v-container>
-    <v-alert type="warning" :value="true">
-      1층 준비중.
-    </v-alert>
+  <v-container grid-list-md text-xs-center>
+    <v-layout row wrap>
+      <v-flex xs4>
+        <v-hover>
+          <v-card
+            slot-scope="{ hover }"
+            class="mx-auto"
+            color="grey lighten-4"
+            max-width="600"
+          >
+            <v-img
+              :aspect-ratio="16/9"
+              src="https://cdn.discordapp.com/attachments/645787220464369670/663708424869249024/20200106_203826.jpg"
+            >
+              <v-expand-transition>
+                <div
+                  v-if="hover"
+                  class="d-flex transition-fast-in-fast-out orange darken-2 v-card--reveal display-3 white--text"
+                  style="height: 100%;"
+                >
+                  $14.99
+                </div>
+              </v-expand-transition>
+            </v-img>
+            <v-card-text
+              class="pt-4"
+              style="position: relative;"
+            >
+              <h3 class="display-1 font-weight-light orange--text mb-2">방송실</h3>
+            </v-card-text>
+          </v-card>
+        </v-hover>
+      </v-flex>
+    <v-spacer></v-spacer>
+      <v-flex xs4>
+        <v-hover>
+          <v-card
+            slot-scope="{ hover }"
+            class="mx-auto"
+            color="grey lighten-4"
+            max-width="600"
+          >
+            <v-img
+              :aspect-ratio="16/9"
+              src="https://cdn.discordapp.com/attachments/645787220464369670/663708381189767186/20200106_203837.jpg"
+            >
+              <v-expand-transition>
+                <div
+                  v-if="hover"
+                  class="d-flex transition-fast-in-fast-out orange darken-2 v-card--reveal display-3 white--text"
+                  style="height: 100%;"
+                >
+                  $14.99
+                </div>
+              </v-expand-transition>
+            </v-img>
+            <v-card-text
+              class="pt-4"
+              style="position: relative;"
+            >
+              <h3 class="display-1 font-weight-light orange--text mb-2">교무실</h3>
+            </v-card-text>
+          </v-card>
+        </v-hover>
+      </v-flex>
+      <v-spacer></v-spacer>
+      <v-flex xs4>
+        <v-hover>
+          <v-card
+            slot-scope="{ hover }"
+            class="mx-auto"
+            color="grey lighten-4"
+            max-width="600"
+          >
+            <v-img
+              :aspect-ratio="16/9"
+              src="https://cdn.discordapp.com/attachments/645787220464369670/663708425808510996/20200106_203810.jpg"
+            >
+              <v-expand-transition>
+                <div
+                  v-if="hover"
+                  class="d-flex transition-fast-in-fast-out orange darken-2 v-card--reveal display-3 white--text"
+                  style="height: 100%;"
+                >
+                  $14.99
+                </div>
+              </v-expand-transition>
+            </v-img>
+            <v-card-text
+              class="pt-4"
+              style="position: relative;"
+            >
+              <h3 class="display-1 font-weight-light orange--text mb-2">행정실</h3>
+            </v-card-text>
+          </v-card>
+        </v-hover>
+      </v-flex>
+      <v-flex xs4>
+        <v-hover>
+          <v-card
+            slot-scope="{ hover }"
+            class="mx-auto"
+            color="grey lighten-4"
+            max-width="600"
+          >
+            <v-img
+              :aspect-ratio="16/9"
+              src="https://cdn.discordapp.com/attachments/645787220464369670/663708498160254979/20200106_203749.jpg"
+            >
+              <v-expand-transition>
+                <div
+                  v-if="hover"
+                  class="d-flex transition-fast-in-fast-out orange darken-2 v-card--reveal display-3 white--text"
+                  style="height: 100%;"
+                >
+                  $14.99
+                </div>
+              </v-expand-transition>
+            </v-img>
+            <v-card-text
+              class="pt-4"
+              style="position: relative;"
+            >
+              <h3 class="display-1 font-weight-light orange--text mb-2">보건실</h3>
+            </v-card-text>
+          </v-card>
+        </v-hover>
+      </v-flex>
+      <!-- <v-spacer></v-spacer>
+      <v-spacer></v-spacer>
+      <v-spacer></v-spacer>
+      <v-spacer></v-spacer> -->
+      <v-flex xs4>
+        <v-hover>
+          <v-card
+            slot-scope="{ hover }"
+            class="mx-auto"
+            color="grey lighten-4"
+            max-width="600"
+          >
+            <v-img
+              :aspect-ratio="16/9"
+              src="https://cdn.discordapp.com/attachments/645787220464369670/663708380044591135/20200106_203904.jpg"
+            >
+              <v-expand-transition>
+                <div
+                  v-if="hover"
+                  class="d-flex transition-fast-in-fast-out orange darken-2 v-card--reveal display-3 white--text"
+                  style="height: 100%;"
+                >
+                  $14.99
+                </div>
+              </v-expand-transition>
+            </v-img>
+            <v-card-text
+              class="pt-4"
+              style="position: relative;"
+            >
+              <h3 class="display-1 font-weight-light orange--text mb-2">sw개발1실</h3>
+            </v-card-text>
+          </v-card>
+        </v-hover>
+      </v-flex>
+      <v-flex xs4>
+        <v-hover>
+          <v-card
+            slot-scope="{ hover }"
+            class="mx-auto"
+            color="grey lighten-4"
+            max-width="600"
+          >
+            <v-img
+              :aspect-ratio="16/9"
+              src="https://cdn.discordapp.com/attachments/645787220464369670/663708318706958336/20200106_203914.jpg"
+            >
+              <v-expand-transition>
+                <div
+                  v-if="hover"
+                  class="d-flex transition-fast-in-fast-out orange darken-2 v-card--reveal display-3 white--text"
+                  style="height: 100%;"
+                >
+                  $14.99
+                </div>
+              </v-expand-transition>
+            </v-img>
+            <v-card-text
+              class="pt-4"
+              style="position: relative;"
+            >
+              <h3 class="display-1 font-weight-light orange--text mb-2">sw개발2실</h3>
+            </v-card-text>
+          </v-card>
+        </v-hover>
+      </v-flex>
+    </v-layout>
   </v-container>
 </template>
+
+<style>
+.v-card--reveal {
+  align-items: center;
+  bottom: 0;
+  justify-content: center;
+  opacity: .5;
+  position: absolute;
+  width: 100%;
+}
+</style>
